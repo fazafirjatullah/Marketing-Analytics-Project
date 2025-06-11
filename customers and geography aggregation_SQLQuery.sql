431367a45aee3c998b5940567069dc2f7e82ca1d
fazafirjatullah <fazaep10@gmail.com>
2025-06-11 21:04:39 +0700

    Add files via upload

---
+++ customers and geography aggregation_SQLQuery.sql
@@ -0,0 +1,15 @@
+SELECT * FROM dbo.customers;
+
+SELECT * FROM dbo.geography;
+
+SELECT
+	c.CustomerID,
+	c.CustomerName,
+	c.Email,
+	c.Gender,
+	c.Age,
+	g.Country,
+	g.City
+FROM dbo.customers c
+LEFT JOIN dbo.geography g
+ON c.GeographyID = g.GeographyID;
